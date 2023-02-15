c30c5d652643d269f635df2567263fdfa1647286
edmundorobertobonequilevine <edmundo.bonequi@coppel.com>
2023-02-15 12:23:24 -0600

    Database and Network created and code to retrieving menu items from network to store in database added

--- app/src/main/java/com/munbonecci/littlelemon/MainActivity.kt
+++ app/src/main/java/com/munbonecci/littlelemon/MainActivity.kt
@@ -10,15 +10,36 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.rememberNavController
+import androidx.room.Room
 import com.munbonecci.littlelemon.Constants.IS_REGISTERED_PREF
 import com.munbonecci.littlelemon.Constants.PREF_NAME
 import com.munbonecci.littlelemon.ui.theme.LittleLemonTheme
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
     private val sharedPreferences by lazy {
         getSharedPreferences(PREF_NAME, MODE_PRIVATE)
+    }
+
+    private val httpClient = HttpClient(Android) {
+        install(ContentNegotiation) {
+            json(contentType = ContentType("text", "plain"))
+        }
+    }
+
+    private val database by lazy {
+        Room.databaseBuilder(applicationContext, AppDatabase::class.java, "database").build()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,6 +58,24 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            if (database.menuItemDao().isEmpty()) {
+                saveMenuToDatabase(fetchMenu())
+            }
+        }
+    }
+
+    private suspend fun fetchMenu(): List<MenuItemNetwork> {
+        val response = httpClient
+            .get("https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json")
+            .body<MenuNetwork>()
+        return response.menu
+    }
+
+    private fun saveMenuToDatabase(menuItemsNetwork: List<MenuItemNetwork>) {
+        val menuItemsRoom = menuItemsNetwork.map { it.toMenuItemRoom() }
+        database.menuItemDao().insertAll(*menuItemsRoom.toTypedArray())
     }
 }
 
